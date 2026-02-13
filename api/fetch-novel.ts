425d5aeee831e5fea0d33701121c4706e19f7532
Rong-bit <hcj382000@gmail.com>
2026-02-13 18:57:33 +0800

    Add files via upload

--- api/fetch-novel.ts
+++ api/fetch-novel.ts
@@ -1,4 +1,4 @@
-import { fetchNovelFromUrl } from './_lib/scraper';
+import { fetchNovelFromUrl } from './_lib/scraper.js';
 
 export default async function handler(req: any, res: any) {
   if (req.method !== 'POST') {
