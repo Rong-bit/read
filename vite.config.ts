7eb82520fa6fa3e906e39305b5f3895170d4cad8
Rong-bit <hcj382000@gmail.com>
2026-04-27 16:10:53 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
+    const isGithubPages = env.GITHUB_PAGES === 'true';
     return {
+      base: isGithubPages ? '/read/' : '/',
       server: {
         port: 3000,
         host: '0.0.0.0',
